78991ac32ba67385640f1b17ca8a19eee3cf16f9
mengyan <iris370924@gmail.com>
2014-10-22 09:45:56 +0000

    edit

--- python/open_txt.py
+++ python/open_txt.py
@@ -1,8 +1,6 @@
 import os
 
-os.getcwd()
-os.chdir('../chapter3')
-os.getcwd()
+os.chdir('chapter3')
 
 """
 Open a file ,and the file content is assigned to a file object called data
@@ -11,5 +9,3 @@ data=open('data.txt')
 for each_line in data:
     print(each_line,end='')
 data.close()
-
-

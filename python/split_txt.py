9cd8a798e563ee6bdd2528e42bcfedb0250e36ef
mengyan <iris370924@gmail.com>
2014-10-23 09:30:45 +0000

    split and try

--- python/split_txt.py
+++ python/split_txt.py
@@ -4,7 +4,13 @@ os.chdir('chapter3')
 data=open('data.txt')
 
 for each_line in data:
-   (role,line_spoken)= each_line.split(':')
-   print(role,end='')
-   print(' said: ',end='')
-   print(line_spoken,end='')
+'''if each_line.find(':')!=-1:   '''
+       try:
+          (role,line_spoken)= each_line.split(':',1)
+          print(role,end='')
+          print(' said: ',end='')
+          print(line_spoken,end='')
+       except:
+          pass
+
+data.close()

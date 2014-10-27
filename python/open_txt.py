341f74143fc378d1348dc573d7ecf7816d657464
mengyan <iris370924@gmail.com>
2014-10-27 08:35:14 +0000

    audit

--- python/open_txt.py
+++ python/open_txt.py
@@ -5,7 +5,11 @@ os.chdir('chapter3')
 """
 Open a file ,and the file content is assigned to a file object called data
 """
-data=open('data.txt')
-for each_line in data:
-    print(each_line,end='')
-data.close()
+
+if os.path.exists('data.txt'):
+    data=open('data.txt')
+    for each_line in data:
+        print(each_line,end='')
+    data.close()
+else:
+    print('the data file is missing')
--- python/split_txt.py
+++ python/split_txt.py
@@ -1,16 +1,18 @@
 import os
 
 os.chdir('chapter3')
-data=open('data.txt')
+try:
+   data=open('data.txt')
 
-for each_line in data:
+   for each_line in data:
 '''if each_line.find(':')!=-1:   '''
-       try:
-          (role,line_spoken)= each_line.split(':',1)
-          print(role,end='')
-          print(' said: ',end='')
-          print(line_spoken,end='')
-       except:
-          pass
-
-data.close()
+      try:
+         (role,line_spoken)= each_line.split(':',1)
+         print(role,end='')
+         print(' said: ',end='')
+         print(line_spoken,end='')
+      except ValueError:
+         pass
+    data.close()
+except IOError:
+    print('the data file is missing')

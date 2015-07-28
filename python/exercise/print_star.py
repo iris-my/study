a13a356ba98802baaaf01da740838681dcceac36
Young <public0821@gmail.com>
2015-07-28 20:09:54 +0800

    change while to for loop

--- python/exercise/print_star.py
+++ python/exercise/print_star.py
@@ -1,13 +1,13 @@
 import math
 import sys
-numbers=input("Please input a number,and it must be posttive integer:")
-#num=int(numbers)
 
-while numbers.isdigit():
-    count=1
-    while (count<=int(numbers)):
-        print('*',end=' ')
-        count=count+1
+numbers = input("Please input a number,and it must be posttive integer:")
+
+if numbers.isdigit():
+    num = int(numbers)
+    for i in range(num):
+        print('*', end=' ')
+    print()
 else:
     print("Your input is not a positive integer!")
 

73a5b5e4695142d4db74633f6b06468664b11fc5
Young <public0821@gmail.com>
2015-07-28 20:20:22 +0800

    let user input again if not input a positive number

--- python/exercise/print_star.py
+++ python/exercise/print_star.py
@@ -1,13 +1,10 @@
-import math
-import sys
+while True:
+    numbers = input("Please input a number,and it must be posttive integer:")
 
-numbers = input("Please input a number,and it must be posttive integer:")
-
-if numbers.isdigit():
-    num = int(numbers)
-    for i in range(num):
-        print('*', end=' ')
-    print()
-else:
-    print("Your input is not a positive integer!")
+    if numbers.isdigit():
+        num = int(numbers)
+        print('*' * num)
+        break
+    else:
+        print("Your input is not a positive integer!")
 

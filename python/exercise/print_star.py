cdebb8e216ab1129c3d52bd846d28b45edc3a9f6
mengyan <iris370924@gmail.com>
2015-07-29 06:06:21 +0000

    print a right triangle

--- python/exercise/print_star.py
+++ python/exercise/print_star.py
@@ -1,13 +1,10 @@
-import math
-import sys
-numbers=input("Please input a number,and it must be posttive integer:")
-#num=int(numbers)
-
-while numbers.isdigit():
-    count=1
-    while (count<=int(numbers)):
-        print('*',end=' ')
-        count=count+1
-else:
-    print("Your input is not a positive integer!")
+while True:
+    numbers=input("Please input a number,and it must be posttive integer:")
+    if numbers.isdigit():
+        num=int(numbers)
+        for i in range(num):
+            print((i+1)*'*')
+        break
+    else:
+        print("Your input is not a positive integer!")
 

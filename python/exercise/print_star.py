840bf28aad3f4dfd5362f2d49c942211001c9007
mengyan <iris370924@gmail.com>
2015-07-30 09:45:35 +0000

    print rhombus

--- python/exercise/print_star.py
+++ python/exercise/print_star.py
@@ -3,7 +3,10 @@ while True:
     if numbers.isdigit():
         num=int(numbers)
         for i in range(num):
-            print('*'*num)
+            print(' '*(num-i)+'*'*(2*i+1))
+        print('*'*(2*num+1))
+        for i in range(num):
+            print(' '*(i+1) + '*'*(2*(num-i-1)+1))
         break
     else:
         print("Your input is not a positive integer!")

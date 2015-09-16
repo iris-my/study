53af9fb96eada12be49852973fe35dad337c5011
mengyan <iris370924@gmail.com>
2015-09-16 07:57:34 +0000

    count the frequency about a word

---
+++ python/exercise/Count_frequency_words.py
@@ -0,0 +1,15 @@
+import os
+import re
+
+words=input("Please input a word you want to search:")
+thefile=open('/PythonStudy_dir/study/python/chapter3/data.txt','r').read()
+
+
+line=re.split('\W',thefile)
+count=0
+for word in (line):
+    if (word==words):
+        count += 1
+print(count)
+
+

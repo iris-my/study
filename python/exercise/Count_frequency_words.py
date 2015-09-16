7610444c1f31d9f9c07878c93430e92c1d40d64e
mengyan <iris370924@gmail.com>
2015-09-16 08:51:38 +0000

    update version of Count_frequency.py

--- python/exercise/Count_frequency_words.py
+++ python/exercise/Count_frequency_words.py
@@ -5,7 +5,7 @@ words=input("Please input a word you want to search:")
 thefile=open('/PythonStudy_dir/study/python/chapter3/data.txt','r').read()
 
 
-line=re.split('\W',thefile)
+line=re.split('[^a-zA-Z0-9\']',thefile)
 count=0
 for word in (line):
     if (word==words):
